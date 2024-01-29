2977db3b4e14b1531a95ef9190251062f25673aa
dmitrimahayana <dmitri.mahayana@gmail.com>
2024-01-29 19:20:14 +0700

    update faiss semantic search

--- haystack_semantics.py
+++ haystack_semantics.py
@@ -2,40 +2,63 @@ from haystack import Pipeline
 from haystack.document_stores import FAISSDocumentStore
 from haystack.nodes import PDFToTextConverter, PreProcessor
 from haystack.nodes import EmbeddingRetriever, PromptNode
+from haystack import Document
 import os
 
 converter = PDFToTextConverter()
 preprocessor = PreProcessor()
+
 db_path = './faiss_document_store.db'
-faiss_index_path= './faiss_document_store.json'
-faiss_config_path = './faiss_document_config'
+faiss_index_path = "./faiss_document_store.faiss"
+faiss_config_path = "./faiss_document_store.json"
+if os.path.exists(db_path):
+  os.remove(db_path)
+if os.path.exists(faiss_index_path):
+  os.remove(faiss_index_path)
+if os.path.exists(faiss_config_path):
+  os.remove(faiss_config_path)
 document_store = FAISSDocumentStore(sql_url=f"sqlite:///{db_path}")
-document_store.save(faiss_index_path, faiss_config_path)
 
-# Add data source
-data_path = './Data/journal_llama2.pdf'
-indexing_pipeline = Pipeline()
-indexing_pipeline.add_node(component=converter, name="PDFConverter", inputs=["File"])
-indexing_pipeline.add_node(component=preprocessor, name="PreProcessor", inputs=["PDFConverter"])
-indexing_pipeline.add_node(component=document_store, name="DocumentStore", inputs=["PreProcessor"])
-indexing_pipeline.run(file_paths=[data_path])
-
-# Add query
-document_store = FAISSDocumentStore.load(index_path=faiss_index_path, config_path=faiss_config_path)
 retriever = EmbeddingRetriever(document_store = document_store,
                                embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
+
 prompt_node = PromptNode(model_name_or_path = "gpt-4",
                          api_key = os.environ.get('CHATGPT_API_KEY'),
                          default_prompt_template = "deepset/question-answering-with-references")
 
+# Create Indexing
+data_path1 = './Data/journal_llama2.pdf'
+data_path2 = './Data/Test.pdf'
+indexing_pipeline = Pipeline()
+indexing_pipeline.add_node(component=converter, name="PDFConverter", inputs=["File"])
+indexing_pipeline.add_node(component=preprocessor, name="PreProcessor", inputs=["PDFConverter"])
+indexing_pipeline.add_node(component=document_store, name="DocumentStore", inputs=["PreProcessor"])
+indexing_pipeline.run(file_paths=[data_path1, data_path2])
+
+# Update Doc Embedding
+document_store.update_embeddings(retriever)
+document_store.save("faiss_document_store.faiss")
+
+# Reload Document Indexing
+document_store = FAISSDocumentStore(
+    faiss_index_path="faiss_document_store.faiss",
+    faiss_config_path="faiss_document_store.json")
+
+# print(document_store.get_all_documents())
+
+# Load and Query Index
 query_pipeline = Pipeline()
 query_pipeline.add_node(component=retriever, name="Retriever", inputs=["Query"])
 query_pipeline.add_node(component=prompt_node, name="PromptNode", inputs=["Retriever"])
 
-question1 = "What is Toxicity?"
-result = query_pipeline.run(query = question1)
-print("Query Result:", result['results'])
+question = "who is dmitri?"
+result = query_pipeline.run(query = question)
+print("\nQuery Result:", result['results'])
 
-question2 = "What is Bias?"
-result = query_pipeline.run(query = question2)
-print("Query Result:", result['results'])
+question = "what is toxicity?"
+result = query_pipeline.run(query = question)
+print("\nQuery Result:", result['results'])
+
+question = "What is bias?"
+result = query_pipeline.run(query = question)
+print("\nQuery Result:", result['results'])
