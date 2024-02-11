10b31e4727913589f1a1c643016f7eb180849a95
dmitrimahayana <dmitri.mahayana@gmail.com>
2024-02-11 17:12:17 +0700

    feat: add ranker moodule

--- haystack_semantics_search.py
+++ haystack_semantics_search.py
@@ -1,8 +1,9 @@
-import pandas as pd
 from haystack import Pipeline
 from haystack.document_stores import FAISSDocumentStore
 from haystack.nodes import EmbeddingRetriever
+from haystack.nodes import SentenceTransformersRanker
 import os
+import pandas as pd
 
 
 def create_index(documents, dict_faiss_config):
@@ -12,9 +13,10 @@ def create_index(documents, dict_faiss_config):
         os.remove(dict_faiss_config['faiss_index_path'])
     if os.path.exists(dict_faiss_config['faiss_config_path']):
         os.remove(dict_faiss_config['faiss_config_path'])
+    # Define Document Store
     document_store = FAISSDocumentStore(sql_url=f"sqlite:///{dict_faiss_config['db_path']}")
 
-    # Retriever Config
+    # Define Retriever
     retriever = EmbeddingRetriever(document_store=document_store,
                                    embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
 
@@ -24,7 +26,7 @@ def create_index(documents, dict_faiss_config):
     document_store.save(dict_faiss_config['faiss_index_path'])
 
 
-def perform_query(query_string, dict_faiss_config):
+def perform_query(query_string, N, dict_faiss_config):
     # Reload Document Indexing
     document_store = FAISSDocumentStore(
         faiss_index_path=dict_faiss_config['faiss_index_path'],
@@ -34,12 +36,19 @@ def perform_query(query_string, dict_faiss_config):
     retriever = EmbeddingRetriever(document_store=document_store,
                                    embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
 
+    # Define Ranker
+    ranker = SentenceTransformersRanker(model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2")
+
     # Create Pipeline
     query_pipeline = Pipeline()
     query_pipeline.add_node(component=retriever, name="Retriever", inputs=["Query"])
+    query_pipeline.add_node(component=ranker, name="Ranker", inputs=["Retriever"])
 
     # Perform Query
-    results = query_pipeline.run(query=query_string, params={"Retriever": {"top_k": 3}})
+    top_k_retriever = N
+    top_k_ranker = N
+
+    results = query_pipeline.run(query=query_string, params={"Ranker": {"top_k": top_k_ranker}, "Retriever": {"top_k": top_k_retriever}})
     print("Query:", query_string)
     for row in results['documents']:
         print(f"ID: {row.id}, Content: {row.content}")
@@ -71,8 +80,8 @@ if __name__ == "__main__":
     documents = df[['id', 'content']].to_dict(orient='records')
 
     # Call Indexing
-    create_index(documents, dict_faiss_config)
+    # create_index(documents, dict_faiss_config)
 
     # Call Search
-    perform_query('fatigue', dict_faiss_config)
-    perform_query('fattigue', dict_faiss_config)
+    perform_query('fatigue', 3, dict_faiss_config)
+    perform_query('fattigue', 3, dict_faiss_config)
