65d7452f9aabf3fd5c689f9cae9526ddf0242f39
dmitrimahayana <dmitri.mahayana@gmail.com>
2024-02-15 10:26:46 +0700

    feat:add es doc store

--- haystack_semantics_search.py
+++ haystack_semantics_search.py
@@ -1,11 +1,11 @@
 from haystack import Pipeline
 from haystack.document_stores import FAISSDocumentStore
+from haystack.document_stores import ElasticsearchDocumentStore
 from haystack.nodes import EmbeddingRetriever
 from haystack.nodes import DensePassageRetriever
 from haystack.nodes import SentenceTransformersRanker
-import os
 import pandas as pd
-
+import os
 
 def create_index(documents, dict_faiss_config):
     # Create Index
@@ -16,52 +16,74 @@ def create_index(documents, dict_faiss_config):
     if os.path.exists(dict_faiss_config['faiss_config_path']):
         os.remove(dict_faiss_config['faiss_config_path'])
     # Define Document Store
-    document_store = FAISSDocumentStore(sql_url=f"sqlite:///{dict_faiss_config['db_path']}",
-                                        embedding_dim=1536,
-                                        faiss_index_factory_str="Flat")
+    # document_store = FAISSDocumentStore(sql_url=f"sqlite:///{dict_faiss_config['db_path']}",
+    #                                     # embedding_dim=1536,
+    #                                     faiss_index_factory_str="Flat")
+    document_store = ElasticsearchDocumentStore(host='localhost',
+                                                port=9200,
+                                                scheme='https',
+                                                verify_certs=True,
+                                                ca_certs='D:/00 Project/00 My Project/docker-elasticsearch/certs/ca/ca.crt',
+                                                api_key_id='-I-Nqo0BPuQqx5wtsBo8',
+                                                api_key='brDDVnBiR7yti6kdeYBZBw',
+                                                embedding_dim = 768, #768 or 1536,
+                                                # index='1536-chatgpt-haystack-search',
+                                                index='768-fb-haystack-search',
+                                                )
 
     # Define Retriever
     # retriever = EmbeddingRetriever(document_store=document_store,
     #                                embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
-    # retriever = DensePassageRetriever(
-    #     document_store=document_store,
-    #     query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-    #     passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base"
-    # )
-    retriever = EmbeddingRetriever(
+    retriever = DensePassageRetriever(
         document_store=document_store,
-        batch_size=8,
-        embedding_model="text-embedding-ada-002",
-        api_key=os.environ.get('CHATGPT_API_KEY'),
-        max_seq_len=1536
+        query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+        passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base"
     )
+    # retriever = EmbeddingRetriever(
+    #     document_store=document_store,
+    #     batch_size=8,
+    #     embedding_model="text-embedding-ada-002",
+    #     api_key=os.environ.get('CHATGPT_API_KEY'),
+    #     max_seq_len=1536
+    # )
 
     # Update Doc Embedding
     document_store.write_documents(documents)
     document_store.update_embeddings(retriever)
-    document_store.save(dict_faiss_config['faiss_index_path'])
+    # document_store.save(dict_faiss_config['faiss_index_path'])
 
 
 def perform_query(query_string, N, dict_faiss_config):
     # Reload Document Indexing
-    document_store = FAISSDocumentStore(faiss_index_path=dict_faiss_config['faiss_index_path'],
-                                        faiss_config_path=dict_faiss_config['faiss_config_path'])
+    # document_store = FAISSDocumentStore(faiss_index_path=dict_faiss_config['faiss_index_path'],
+    #                                     faiss_config_path=dict_faiss_config['faiss_config_path'])
+    document_store = ElasticsearchDocumentStore(host='localhost',
+                                                port=9200,
+                                                scheme='https',
+                                                verify_certs=True,
+                                                ca_certs='D:/00 Project/00 My Project/docker-elasticsearch/certs/ca/ca.crt',
+                                                api_key_id='-I-Nqo0BPuQqx5wtsBo8',
+                                                api_key='brDDVnBiR7yti6kdeYBZBw',
+                                                embedding_dim = 768, #768 or 1536
+                                                # index='1536-chatgpt-haystack-search',
+                                                index='768-fb-haystack-search',
+                                                )
 
     # Retriever Config
     # retriever = EmbeddingRetriever(document_store=document_store,
     #                                embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
-    # retriever = DensePassageRetriever(
-    #     document_store=document_store,
-    #     query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
-    #     passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base"
-    # )
-    retriever = EmbeddingRetriever(
+    retriever = DensePassageRetriever(
         document_store=document_store,
-        batch_size=8,
-        embedding_model="text-embedding-ada-002",
-        api_key=os.environ.get('CHATGPT_API_KEY'),
-        max_seq_len=1536
+        query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+        passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base"
     )
+    # retriever = EmbeddingRetriever(
+    #     document_store=document_store,
+    #     batch_size=8,
+    #     embedding_model="text-embedding-ada-002",
+    #     api_key=os.environ.get('CHATGPT_API_KEY'),
+    #     max_seq_len=1536
+    # )
 
     # Define Ranker
     ranker = SentenceTransformersRanker(model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2")
@@ -90,19 +112,20 @@ def perform_query(query_string, N, dict_faiss_config):
     print("Done...")
 
 
-def update_index(dict_faiss_config, documents):
-    # Reload Document Indexing
-    document_store = FAISSDocumentStore(faiss_index_path=dict_faiss_config['faiss_index_path'],
-                                        faiss_config_path=dict_faiss_config['faiss_config_path'])
+# def update_index(dict_faiss_config, documents):
+#     # Reload Document Indexing
+#     document_store = FAISSDocumentStore(faiss_index_path=dict_faiss_config['faiss_index_path'],
+#                                         faiss_config_path=dict_faiss_config['faiss_config_path'])
+#     # document_store = ElasticsearchDocumentStore(hosts="http://localhost:9200")
 
-    # Define Retriever
-    retriever = EmbeddingRetriever(document_store=document_store,
-                                   embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
+#     # Define Retriever
+#     retriever = EmbeddingRetriever(document_store=document_store,
+#                                    embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
 
-    # Update Doc Embedding
-    document_store.write_documents(documents)
-    document_store.update_embeddings(retriever)
-    document_store.save(dict_faiss_config['faiss_index_path'])
+#     # Update Doc Embedding
+#     document_store.write_documents(documents)
+#     document_store.update_embeddings(retriever)
+#     document_store.save(dict_faiss_config['faiss_index_path'])
 
 
 if __name__ == "__main__":
@@ -130,10 +153,10 @@ if __name__ == "__main__":
     documents1 = df[['id', 'content']].to_dict(orient='records')
 
     # Call Indexing
-    # create_index(documents1, dict_faiss_config)
+    create_index(documents1, dict_faiss_config)
 
     # Call Search
-    perform_query('fattigue', 3, dict_faiss_config)
+    # perform_query('fatigue adult', 3, dict_faiss_config)
     perform_query('fatigguee', 3, dict_faiss_config)
 
     # # Sample DataFrame 2
