cf9ee062cb360d3d8bb0bf20a763118642ff402a
dmitrimahayana <dmitri.mahayana@gmail.com>
2024-03-10 11:55:43 +0700

    update es

--- haystack_semantics_search.py
+++ haystack_semantics_search.py
@@ -4,32 +4,49 @@ from haystack.document_stores import ElasticsearchDocumentStore
 from haystack.nodes import EmbeddingRetriever
 from haystack.nodes import DensePassageRetriever
 from haystack.nodes import SentenceTransformersRanker
+from dotenv import load_dotenv
 import pandas as pd
 import os
+import ast
 
-def create_index(documents, dict_faiss_config):
-    # Create Index
-    if os.path.exists(dict_faiss_config['db_path']):
-        os.remove(dict_faiss_config['db_path'])
-    if os.path.exists(dict_faiss_config['faiss_index_path']):
-        os.remove(dict_faiss_config['faiss_index_path'])
-    if os.path.exists(dict_faiss_config['faiss_config_path']):
-        os.remove(dict_faiss_config['faiss_config_path'])
-    # Define Document Store
-    # document_store = FAISSDocumentStore(sql_url=f"sqlite:///{dict_faiss_config['db_path']}",
-    #                                     # embedding_dim=1536,
+# Define env variable
+load_dotenv(".env")
+
+FAISS_DB_PATH = os.getenv('faiss_db_path')
+FAISS_INDEX_PATH = os.getenv('faiss_index_path')
+FAISS_CONFIG_PATH = os.getenv('faiss_config_path')
+ES_HOST = os.getenv('es_host')
+ES_PORT = os.getenv('es_port')
+ES_SCHEME = os.getenv('es_scheme')
+ES_VERIFY_CERTS = os.getenv('es_verify_certs')
+ES_CA_CERTS = os.getenv('es_ca_certs')
+ES_USERNAME = os.getenv('es_username')
+ES_PASSWORD = os.getenv('es_password')
+ES_EMBEDDING_DIM = os.getenv('es_embedding_dim')
+ES_PREFIX_INDEX = os.getenv('es_prefix_index')
+
+
+def create_index(documents):
+    # Delete Existing Faiss Store
+    # if os.path.exists(FAISS_DB_PATH):
+    #     os.remove(FAISS_DB_PATH)
+    # if os.path.exists(FAISS_INDEX_PATH):
+    #     os.remove(FAISS_INDEX_PATH)
+    # if os.path.exists(FAISS_CONFIG_PATH):
+    #     os.remove(FAISS_CONFIG_PATH)
+    # Create Store
+    # document_store = FAISSDocumentStore(sql_url=f"sqlite:///{FAISS_DB_PATH}",
+    #                                     embedding_dim=768,  # 768 OR 1536
     #                                     faiss_index_factory_str="Flat")
-    document_store = ElasticsearchDocumentStore(host='localhost',
-                                                port=9200,
-                                                scheme='https',
-                                                verify_certs=True,
-                                                ca_certs='D:/00 Project/00 My Project/docker-elasticsearch/certs/ca/ca.crt',
-                                                api_key_id='-I-Nqo0BPuQqx5wtsBo8',
-                                                api_key='brDDVnBiR7yti6kdeYBZBw',
-                                                embedding_dim = 768, #768 or 1536,
-                                                # index='1536-chatgpt-haystack-search',
-                                                index='768-fb-haystack-search',
-                                                )
+    document_store = ElasticsearchDocumentStore(host=ES_HOST,
+                                                port=ES_PORT,
+                                                scheme=ES_SCHEME,
+                                                verify_certs=ast.literal_eval(ES_VERIFY_CERTS),
+                                                ca_certs=ES_CA_CERTS,
+                                                username=ES_USERNAME,
+                                                password=ES_PASSWORD,
+                                                embedding_dim=int(ES_EMBEDDING_DIM),
+                                                index=ES_PREFIX_INDEX+'_fb_'+ES_EMBEDDING_DIM)
 
     # Define Retriever
     # retriever = EmbeddingRetriever(document_store=document_store,
@@ -39,37 +56,28 @@ def create_index(documents, dict_faiss_config):
         query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
         passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base"
     )
-    # retriever = EmbeddingRetriever(
-    #     document_store=document_store,
-    #     batch_size=8,
-    #     embedding_model="text-embedding-ada-002",
-    #     api_key=os.environ.get('CHATGPT_API_KEY'),
-    #     max_seq_len=1536
-    # )
 
     # Update Doc Embedding
     document_store.write_documents(documents)
     document_store.update_embeddings(retriever)
-    # document_store.save(dict_faiss_config['faiss_index_path'])
+    # document_store.save(FAISS_INDEX_PATH)
 
 
-def perform_query(query_string, N, dict_faiss_config):
-    # Reload Document Indexing
-    # document_store = FAISSDocumentStore(faiss_index_path=dict_faiss_config['faiss_index_path'],
-    #                                     faiss_config_path=dict_faiss_config['faiss_config_path'])
-    document_store = ElasticsearchDocumentStore(host='localhost',
-                                                port=9200,
-                                                scheme='https',
-                                                verify_certs=True,
-                                                ca_certs='D:/00 Project/00 My Project/docker-elasticsearch/certs/ca/ca.crt',
-                                                api_key_id='-I-Nqo0BPuQqx5wtsBo8',
-                                                api_key='brDDVnBiR7yti6kdeYBZBw',
-                                                embedding_dim = 768, #768 or 1536
-                                                # index='1536-chatgpt-haystack-search',
-                                                index='768-fb-haystack-search',
-                                                )
+def perform_query(query_string, N):
+    # Reload Document Store
+    # document_store = FAISSDocumentStore(faiss_index_path=FAISS_INDEX_PATH,
+    #                                     faiss_config_path=FAISS_CONFIG_PATH)
+    document_store = ElasticsearchDocumentStore(host=ES_HOST,
+                                                port=ES_PORT,
+                                                scheme=ES_SCHEME,
+                                                verify_certs=ast.literal_eval(ES_VERIFY_CERTS),
+                                                ca_certs=ES_CA_CERTS,
+                                                username=ES_USERNAME,
+                                                password=ES_PASSWORD,
+                                                embedding_dim=int(ES_EMBEDDING_DIM),
+                                                index=ES_PREFIX_INDEX+'_fb_'+ES_EMBEDDING_DIM)
 
-    # Retriever Config
+    # Define Retriever
     # retriever = EmbeddingRetriever(document_store=document_store,
     #                                embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
     retriever = DensePassageRetriever(
@@ -77,20 +85,9 @@ def perform_query(query_string, N, dict_faiss_config):
         query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
         passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base"
     )
-    # retriever = EmbeddingRetriever(
-    #     document_store=document_store,
-    #     batch_size=8,
-    #     embedding_model="text-embedding-ada-002",
-    #     api_key=os.environ.get('CHATGPT_API_KEY'),
-    #     max_seq_len=1536
-    # )
 
     # Define Ranker
     ranker = SentenceTransformersRanker(model_name_or_path="cross-encoder/ms-marco-MiniLM-L-12-v2")
-    # ranker = LostInTheMiddleRanker(
-    #     word_count_threshold=1024,
-    #     top_k=N,
-    # )
 
     # Create Pipeline
     query_pipeline = Pipeline()
@@ -101,83 +98,63 @@ def perform_query(query_string, N, dict_faiss_config):
     top_k_retriever = N
     top_k_ranker = N
 
-    # results = query_pipeline.run(query=query_string,
-    #                              params={"Retriever": {"top_k": top_k_retriever}, "Ranker": {"top_k": top_k_ranker}})
-    # results = query_pipeline.run(query=query_string,
-    #                              params={"Ranker": {"top_k": top_k_ranker}})
-    results = query_pipeline.run(query=query_string)
+    results = query_pipeline.run(query=query_string,
+                                 params={"Retriever": {"top_k": top_k_retriever}, "Ranker": {"top_k": top_k_ranker}})
     print("Query:", query_string)
     for row in results['documents']:
-        print(f"ID: {row.id}, Content: {row.content}")
+        print(f"ID: {row.id}, Content: {row.content[:100]}, Score: {row.score}")
     print("Done...")
 
 
-# def update_index(dict_faiss_config, documents):
-#     # Reload Document Indexing
-#     document_store = FAISSDocumentStore(faiss_index_path=dict_faiss_config['faiss_index_path'],
-#                                         faiss_config_path=dict_faiss_config['faiss_config_path'])
-#     # document_store = ElasticsearchDocumentStore(hosts="http://localhost:9200")
+def update_index(dict_faiss_config, documents):
+    # Reload Document Store
+    # document_store = FAISSDocumentStore(faiss_index_path=FAISS_INDEX_PATH,
+    #                                     faiss_config_path=FAISS_CONFIG_PATH)
+    document_store = ElasticsearchDocumentStore(host=ES_HOST,
+                                                port=ES_PORT,
+                                                scheme=ES_SCHEME,
+                                                verify_certs=ast.literal_eval(ES_VERIFY_CERTS),
+                                                ca_certs=ES_CA_CERTS,
+                                                username=ES_USERNAME,
+                                                password=ES_PASSWORD,
+                                                embedding_dim=int(ES_EMBEDDING_DIM),
+                                                index=ES_PREFIX_INDEX+'_fb_'+ES_EMBEDDING_DIM)
 
-#     # Define Retriever
-#     retriever = EmbeddingRetriever(document_store=document_store,
-#                                    embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
+    # Define Retriever
+    # retriever = EmbeddingRetriever(document_store=document_store,
+    #                                embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1")
+    retriever = DensePassageRetriever(
+        document_store=document_store,
+        query_embedding_model="facebook/dpr-question_encoder-single-nq-base",
+        passage_embedding_model="facebook/dpr-ctx_encoder-single-nq-base"
+    )
 
-#     # Update Doc Embedding
-#     document_store.write_documents(documents)
-#     document_store.update_embeddings(retriever)
-#     document_store.save(dict_faiss_config['faiss_index_path'])
+    # Update Doc Embedding
+    document_store.write_documents(documents)
+    document_store.update_embeddings(retriever)
+    # document_store.save(FAISS_INDEX_PATH)
 
 
 if __name__ == "__main__":
-    # Document Store Config
-    dict_faiss_config = {
-        'db_path': 'faiss/faiss_dataframe_document_store.db',
-        'faiss_index_path': 'faiss/faiss_dataframe_document_store.faiss',
-        'faiss_config_path': 'faiss/faiss_dataframe_document_store.json'
-    }
-
-    # Sample DataFrame 1
+    # Sample Data
     data = {
-        'id': [1, 2, 3, 4, 5],
-        'url': ['url1', 'url2', 'url3', 'url4', 'url5'],
-        'title': ['apar', 'cek fatigue', 'microsleep', 'check fatigue', 'lingkungan berdebu'],
-        'description': ['Apar Low Pressure unit CO2470',
-                        'pemeriksaan kebugaran dan keamanan unit',
-                        'microsleep fatigue',
-                        'management fatigue',
-                        'jalan hauling berdebu'],
-        'location': ['loc1', 'loc2', 'loc3', 'loc4', 'loc5']
+        'id': [10001, 10002, 10003, 10004, 10005],
+        'url': ['https://docs.haystack.deepset.ai/docs/10001', 'https://docs.haystack.deepset.ai/docs/10002',
+                'https://docs.haystack.deepset.ai/docs/10003', 'https://docs.haystack.deepset.ai/docs/10004',
+                'https://docs.haystack.deepset.ai/docs/10005'],
+        'header': ['semantic search', 'search', 'semantic engine', 'ultimate semantic', 'search engine'],
+        'body': ['Haystack is an open-source framework for building production-ready LLM applications',
+                 'retrieval-augmented generative pipelines and state-of-the-art search systems that work intelligently over large document collections',
+                 'Haystack is an end-to-end framework that you can use to build powerful and production-ready pipelines with Large Language Models (LLMs) for different search use cases',
+                 'Whether you want to perform retrieval-augmented generation (RAG), question answering, or semantic document search, you can use the state-of-the-art LLMs and NLP models in Haystack to provide custom search experiences and make it possible for your users to query in natural language',
+                 'Haystack is built in a modular fashion so that you can combine the best technology from OpenAI, Cohere, SageMaker, and other open source projects, like Hugging Face\'s Transformers, Elasticsearch, or Milvus']
     }
     df = pd.DataFrame(data)
-    df['content'] = df['title'] + ' ' + df['description']
-    documents1 = df[['id', 'content']].to_dict(orient='records')
+    df['content'] = df['header'] + ' ' + df['body']
+    documents = df[['id', 'content']].to_dict(orient='records')
 
-    # Call Indexing
-    create_index(documents1, dict_faiss_config)
+    # Perform Indexing
+    create_index(documents)
 
-    # Call Search
-    # perform_query('fatigue adult', 3, dict_faiss_config)
-    perform_query('fatigguee', 3, dict_faiss_config)
-
-    # # Sample DataFrame 2
-    # data = {
-    #     'id': [6, 7, 8, 9, 10],
-    #     'url': ['url6', 'url7', 'url8', 'url9', 'url10'],
-    #     'title': ['apar', 'cek fatigue', 'microsleep', 'check fatigue', 'lingkungan berdebu'],
-    #     'description': ['Apar Low Pressure unit CO2470',
-    #                     'pemeriksaan kebugaran dan keamanan unit',
-    #                     'microsleep fatigue',
-    #                     'management fatigue',
-    #                     'jalan hauling berdebu'],
-    #     'location': ['loc6', 'loc7', 'loc8', 'loc9', 'loc10']
-    # }
-    # df = pd.DataFrame(data)
-    # df['content'] = df['title'] + ' ' + df['description']
-    # documents2 = df[['id', 'content']].to_dict(orient='records')
-    #
-    # # Update Indexing
-    # update_index(dict_faiss_config, documents2)
-    #
-    # # Call Search
-    # perform_query('fatigue', 6, dict_faiss_config)
-    # perform_query('fattigue', 6, dict_faiss_config)
+    # Perform Searching
+    perform_query('Haystack', 3, )
